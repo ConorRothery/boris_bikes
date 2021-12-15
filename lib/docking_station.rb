d7f7bc8bb78c10077590ec0c8009f972d1ff3ecb
Conor Rothery <conor.rothery@hotmail.co.uk>
2021-12-15 16:00:21 +0000

    Fourth Keyboard

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,8 @@
 class DockingStation 
   def release_bike
-    
+    docking_station = Bike.new 
+  end
+  def dock_bike
+  
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,6 +2,10 @@ require "docking_station"
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
-
+  it "Its releases a working bike!" do 
+    docking_station = DockingStation.new
+    docking_station.release_bike
+    expect(docking_station.release_bike.working?).to eq("The bike is working!")
+  end
+  it { is_expected.to respond_to :dock_bike }
 end
- 
